219da38b61ca70f838e262490b3e8fd4ca11210f
HuangXiao <hx36w35@163.com>
2020-09-17 23:01:42 +0800

    Add union find code

---
+++ src/cn/edu/nju/unionfind/UF.java
@@ -0,0 +1,51 @@
+package cn.edu.nju.unionfind;
+
+public class UF {
+
+    private int count;
+    private final int[] parent;
+    private final int[] size;
+
+    public UF(int n) {
+        this.count = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (x != parent[x])
+            parent[x] = find(x);
+        return parent[x];
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        // union by rank
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+
+    public int count() {
+        return count;
+    }
+
+}
