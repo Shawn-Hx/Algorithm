b32c9af9f82084483746911536816511078c76d8
HuangXiao <hx36w35@163.com>
2020-09-20 20:20:28 +0800

    Add longest increasing subsequence (LIS) code

---
+++ src/cn/edu/nju/dp/LIS.java
@@ -0,0 +1,43 @@
+package cn.edu.nju.dp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * LeetCode 300. Longest Increasing Subsequence
+ * 最长上升子序列
+ */
+public class LIS {
+
+    // O(nlogn)
+    public int advanceLengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        int res = 0;
+        for (int num : nums) {
+            int index = Arrays.binarySearch(dp, num);
+            if (index >= 0)
+                continue;
+            dp[-index-1] = num;
+            res = Math.max(res, -index);
+        }
+        return res;
+    }
+
+    // O(n^2)
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--)
+                if (nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+}
